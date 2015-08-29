c19645ec4c16c0a3cf4f78fc03e7fcf803524532
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-08-30 02:05:26 +0700

    add regenerate method

--- Regenerate-thumbnail.php
+++ Regenerate-thumbnail.php
@@ -14,4 +14,35 @@ class RegenerateThumbnail{
 	public function __construct(){
 
 	}
+	public function regenerate($attachmentId){
+
+		if(!current_user_can('manage_options')){
+			return new WP_Error('NOT_PERMISSION', 'Only admin can regenerate image.');
+		}
+		$post = get_post($attachmentId);
+		if(!$post){
+			return new WP_Error('POST_NOT_EXIST', 'This postId is not exist.');
+		}
+		if('attachment' != $post->post_type || !preg_match('!^image/!', get_post_mime_type( $post ) ) ){
+			return new WP_Error('NOT_SUPPORT', 'This plugin is only support for image attachment');
+		}
+
+		$fullSizePath = get_attached_file( $post->ID );
+		if(!file_exists($fullSizePath)){
+			return new WP_Error('NOT_DISPLAYABLE', 'This attachment\'s file is not displayable');
+		}
+		if(!function_exists('wp_generate_attachment_metadata')){
+			include( ABSPATH . 'wp-admin/includes/image.php' );
+		}
+		$metaData = wp_generate_attachment_metadata( $post->ID, $fullSizePath );
+		if(is_wp_error($metaData)){
+			return $metaData;
+		}
+		if ( empty( $metaData ) ){
+			return new WP_Error('UNKNOW_ERRORR', 'Unknown error !');
+		}
+		wp_update_attachment_metadata( $post->ID, $metaData );
+		return true;;
+	}
+}
 }
