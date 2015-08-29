e6d4f5d60d76a9b9fd0f8015d6972de8779ccd58
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-08-30 01:32:13 +0700

    init plugin

---
+++ Regenerate-thumbnail.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Regenerate Thumbnail
+Plugin URI: http://laptrinh.senviet.org
+Description: Allow you to regenerate the attachment thumbnail and sizes.
+Version: 1.0
+Author: nguyenvanduocit
+Author URI: http://nvduoc@senviet.org
+License: GPL2
+*/
+
