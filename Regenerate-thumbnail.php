0cdeb87dcd377f6077c44b2e48ea52c12373d0b9
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-08-30 02:33:21 +0700

    add action link for media

--- Regenerate-thumbnail.php
+++ Regenerate-thumbnail.php
@@ -10,38 +10,51 @@ Author URI: http://nvduoc@senviet.org
 License: GPL2
 */
 
-class RegenerateThumbnail{
-	public function __construct(){
-
+class RegenerateThumbnail {
+	public function __construct() {
+		add_filter( 'media_row_actions', array( $this, 'add_media_row_action' ), 10, 2 );
 	}
-	public function regenerate($attachmentId){
 
-		if(!current_user_can('manage_options')){
-			return new WP_Error('NOT_PERMISSION', 'Only admin can regenerate image.');
+	public function regenerate( $attachmentId ) {
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return new WP_Error( 'NOT_PERMISSION', 'Only admin can regenerate image.' );
 		}
-		$post = get_post($attachmentId);
-		if(!$post){
-			return new WP_Error('POST_NOT_EXIST', 'This postId is not exist.');
+		$post = get_post( $attachmentId );
+		if ( ! $post ) {
+			return new WP_Error( 'POST_NOT_EXIST', 'This postId is not exist.' );
 		}
-		if('attachment' != $post->post_type || !preg_match('!^image/!', get_post_mime_type( $post ) ) ){
-			return new WP_Error('NOT_SUPPORT', 'This plugin is only support for image attachment');
+		if ( 'attachment' != $post->post_type || ! preg_match( '!^image/!', get_post_mime_type( $post ) ) ) {
+			return new WP_Error( 'NOT_SUPPORT', 'This plugin is only support for image attachment' );
 		}
 
 		$fullSizePath = get_attached_file( $post->ID );
-		if(!file_exists($fullSizePath)){
-			return new WP_Error('NOT_DISPLAYABLE', 'This attachment\'s file is not displayable');
+		if ( ! file_exists( $fullSizePath ) ) {
+			return new WP_Error( 'NOT_DISPLAYABLE', 'This attachment\'s file is not displayable' );
 		}
-		if(!function_exists('wp_generate_attachment_metadata')){
+		if ( ! function_exists( 'wp_generate_attachment_metadata' ) ) {
 			include( ABSPATH . 'wp-admin/includes/image.php' );
 		}
 		$metaData = wp_generate_attachment_metadata( $post->ID, $fullSizePath );
-		if(is_wp_error($metaData)){
+		if ( is_wp_error( $metaData ) ) {
 			return $metaData;
 		}
-		if ( empty( $metaData ) ){
-			return new WP_Error('UNKNOW_ERRORR', 'Unknown error !');
+		if ( empty( $metaData ) ) {
+			return new WP_Error( 'UNKNOW_ERRORR', 'Unknown error !' );
 		}
 		wp_update_attachment_metadata( $post->ID, $metaData );
-		return true;;
+
+		return true;
+	}
+
+	public function add_media_row_action( $actions, $post ) {
+		if ( !preg_match( '!^image/!', get_post_mime_type( $post ) ) || ! current_user_can( 'manage_options' ) ) {
+			return $actions;
+		}
+		$actions['regenerate_thumbnails'] = '<a data-id="'.$post->ID.'" class="regenerate_thumbnail" href="#" title="' . esc_attr( __( "Regenerate the thumbnails for this single image", 'regenerate-thumbnails' ) ) . '">' . __( 'Regenerate Thumbnails', 'regenerate-thumbnails' ) . '</a>';
+
+		return $actions;
 	}
 }
+
+new RegenerateThumbnail();
