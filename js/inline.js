bcb87949f59a64e5f3c4214a67dabc3a0a88aa77
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-08-30 12:16:53 +0700

    refactor

--- js/inline.js
+++ js/inline.js
@@ -3,17 +3,24 @@
         $( document ).ready(function() {
             $(document).on('click', '.regenerate_thumbnail', function(){
                 var postId = $(this).data('id');
+                requestRegenerate(postId, function(data){
+                    alert(data.message);
+                })
+            });
+            function requestRegenerate(id, successCallback){
                 $.ajax({
                     method: "POST",
                     url: ajaxurl ,
                     data: {
                         action:'regeneratethumbnail',
-                        attachmentId: postId
+                        attachmentId: id
                     }
-                 }).success(function( data ) {
-                        alert(data.message );
+                }).success(function( data ) {
+                    if(successCallback){
+                        successCallback.call(this, data);
+                    }
                 });
-            });
+            }
         });
     }
 )(jQuery);
